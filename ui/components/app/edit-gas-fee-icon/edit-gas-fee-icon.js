7310972604519c9bdb105a5f588e0738586f674c
Olusegun Akintayo <akintayo.segun@gmail.com>
2023-08-24 10:17:23 +0300

    Make some changes to edit gas fee icon
    Signed-off-by: Olusegun Akintayo <akintayo.segun@gmail.com>

--- ui/components/app/edit-gas-fee-icon/edit-gas-fee-icon.js
+++ ui/components/app/edit-gas-fee-icon/edit-gas-fee-icon.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import { useGasFeeContext } from '../../../contexts/gasFee';
 import { useTransactionEventFragment } from '../../../hooks/useTransactionEventFragment';
 import { useTransactionModalContext } from '../../../contexts/transaction-modal';
-import { ButtonIcon, ButtonIconSize, IconName } from '../../component-library';
+import { BUTTON_VARIANT, Button, ButtonIconSize, IconName, Text } from '../../component-library';
+import { IconColor, Size, TextVariant } from '../../../helpers/constants/design-system';
 
 export default function EditGasFeeIcon({ userAcknowledgedGasMissing }) {
   const { hasSimulationError, estimateUsed, supportsEIP1559 } =
@@ -26,12 +27,14 @@ export default function EditGasFeeIcon({ userAcknowledgedGasMissing }) {
   };
 
   return (
-    <ButtonIcon
-      ariaLabel="edit"
-      iconName={IconName.Edit}
-      size={ButtonIconSize.Sm}
-      onClick={openEditGasFeeModal}
+    <Button
+      style={{ textDecoration: 'none' }}
+      size={Size.SM}
+      variant={BUTTON_VARIANT.LINK}
+      startIconName={IconName.Edit}
+      color={IconColor.primaryDefault}
       data-testid="edit-gas-fee-icon"
+      onClick={openEditGasFeeModal}
     />
   );
 }
